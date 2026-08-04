a3668b4fc405a695411b388d8da4c24950ce87ef
hungnmph50619 <hungnmph50619@gmail.com>
2026-08-05 04:13:02 +0700

    Điều chỉnh kiểm tra đồng ý điều khoản

--- src/SmartCar.Web/ViewModels/RegisterViewModel.cs
+++ src/SmartCar.Web/ViewModels/RegisterViewModel.cs
@@ -30,7 +30,6 @@ public class RegisterViewModel
     [Display(Name = "Xác nhận mật khẩu")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
-    [Range(typeof(bool), "true", "true", ErrorMessage = "Bạn cần đồng ý với điều khoản sử dụng và chính sách bảo mật.")]
     [Display(Name = "Đồng ý điều khoản")]
     public bool AcceptTerms { get; set; }
 }
