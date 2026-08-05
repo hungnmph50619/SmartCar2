0625623d1ae40e0c32386f740fa3596ee4c73369
hungnmph50619 <hungnmph50619@gmail.com>
2026-08-05 12:25:47 +0700

    refactor(auth): đổi hằng role Manager thành Admin

--- src/SmartCar.Domain/Constants/RoleNames.cs
+++ src/SmartCar.Domain/Constants/RoleNames.cs
@@ -3,5 +3,5 @@ namespace SmartCar.Domain.Constants;
 public static class RoleNames
 {
     public const string Customer = "Customer";
-    public const string Manager = "Manager";
+    public const string Admin = "Admin";
 }
