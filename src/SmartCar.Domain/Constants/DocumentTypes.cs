a56654e743046340be178369f787b7221e8aa41f
hungnmph50619 <hungnmph50619@gmail.com>
2026-08-07 16:50:58 +0700

    Bổ sung GPLX mặt sau cho KYC

--- src/SmartCar.Domain/Constants/DocumentTypes.cs
+++ src/SmartCar.Domain/Constants/DocumentTypes.cs
@@ -2,13 +2,13 @@ namespace SmartCar.Domain.Constants;
 
 public static class DocumentTypes
 {
-    // Giữ nguyên giá trị "CCCD" để tương thích với dữ liệu đã có.
-    // Trong giao diện, loại này được hiểu là CCCD mặt trước.
+    // Giữ nguyên giá trị cũ để tương thích với dữ liệu hiện có.
     public const string CitizenId = "CCCD";
     public const string CitizenIdentityCard = CitizenId;
     public const string CitizenIdBack = "CCCD mặt sau";
     public const string DrivingLicense = "GPLX";
+    public const string DrivingLicenseBack = "GPLX mặt sau";
 
     public static readonly IReadOnlyCollection<string> RequiredForRental =
-        new[] { CitizenId, CitizenIdBack, DrivingLicense };
+        new[] { CitizenId, CitizenIdBack, DrivingLicense, DrivingLicenseBack };
 }
