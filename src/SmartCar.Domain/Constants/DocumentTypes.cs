e16f18e2835e6d31d7e1b9706d501689e7a0b764
hungnmph50619 <hungnmph50619@gmail.com>
2026-08-07 08:56:44 +0700

    Bổ sung CCCD mặt sau cho hồ sơ thuê xe

--- src/SmartCar.Domain/Constants/DocumentTypes.cs
+++ src/SmartCar.Domain/Constants/DocumentTypes.cs
@@ -2,10 +2,13 @@ namespace SmartCar.Domain.Constants;
 
 public static class DocumentTypes
 {
+    // Giữ nguyên giá trị "CCCD" để tương thích với dữ liệu đã có.
+    // Trong giao diện, loại này được hiểu là CCCD mặt trước.
     public const string CitizenId = "CCCD";
     public const string CitizenIdentityCard = CitizenId;
+    public const string CitizenIdBack = "CCCD mặt sau";
     public const string DrivingLicense = "GPLX";
 
     public static readonly IReadOnlyCollection<string> RequiredForRental =
-        new[] { CitizenId, DrivingLicense };
+        new[] { CitizenId, CitizenIdBack, DrivingLicense };
 }
