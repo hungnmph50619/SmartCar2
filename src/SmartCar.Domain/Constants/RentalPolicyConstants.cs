323e140b8816a43f9bcc21854e0fdf147d08b374
HoangQuySeven <nguyenhoagquy79@gmail.com>
2026-08-15 13:33:33 +0700

    Quý: thêm chính sách cọc bảo đảm

---
+++ src/SmartCar.Domain/Constants/RentalPolicyConstants.cs
@@ -0,0 +1,9 @@
+namespace SmartCar.Domain.Constants;
+
+public static class RentalPolicyConstants
+{
+    public const decimal SecurityDepositAmount = 5_000_000m;
+    public const string SecurityDepositCashMethod = "Tiền mặt";
+    public const string SecurityDepositTransferMethod = "Chuyển khoản";
+    public const string SecurityDepositRefundMethod = "Hoàn cọc bảo đảm";
+}
