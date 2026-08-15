dc4b3482c8a1798fdc533592cf9cef03b2a5b73c
HoangQuySeven <nguyenhoagquy79@gmail.com>
2026-08-15 13:36:19 +0700

    Quý: bổ sung phương thức quyết toán cọc

--- src/SmartCar.Domain/Constants/RentalPolicyConstants.cs
+++ src/SmartCar.Domain/Constants/RentalPolicyConstants.cs
@@ -5,5 +5,7 @@ public static class RentalPolicyConstants
     public const decimal SecurityDepositAmount = 5_000_000m;
     public const string SecurityDepositCashMethod = "Tiền mặt";
     public const string SecurityDepositTransferMethod = "Chuyển khoản";
+    public const string SecurityDepositSettlementMethod = "Đối trừ tiền cọc";
+    public const string SecurityDepositMixedSettlementMethod = "Đối trừ cọc + thanh toán bổ sung";
     public const string SecurityDepositRefundMethod = "Hoàn cọc bảo đảm";
 }
