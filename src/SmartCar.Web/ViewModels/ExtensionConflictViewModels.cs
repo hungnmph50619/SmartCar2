c7fa68a841c8c3ed5b1966c0f2ebb88b17b92990
HoangQuySeven <nguyenhoagquy79@gmail.com>
2026-08-17 18:38:10 +0700

    Remove obsolete automatic compensation fields

--- src/SmartCar.Web/ViewModels/ExtensionConflictViewModels.cs
+++ src/SmartCar.Web/ViewModels/ExtensionConflictViewModels.cs
@@ -9,11 +9,6 @@ public sealed class ExtensionConflictResolutionViewModel
     public string LicensePlate { get; init; } = string.Empty;
     public DateTime PickupDate { get; init; }
     public DateTime ReturnDate { get; init; }
-    public decimal ContractAmount { get; init; }
-    public decimal DepositPaidByCurrentRenter { get; init; }
-    public bool HasPaidUpfront { get; init; }
-    public decimal SuggestedCompensationAmount =>
-        Math.Min(ContractAmount, DepositPaidByCurrentRenter);
 
     public IReadOnlyList<ExtensionAlternativeVehicleViewModel> Alternatives { get; init; }
         = Array.Empty<ExtensionAlternativeVehicleViewModel>();
