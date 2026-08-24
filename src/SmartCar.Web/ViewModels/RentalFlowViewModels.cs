fdc5bc4849558560c0c39d0b180159274396414d
HoangQuySeven <nguyenhoagquy79@gmail.com>
2026-08-24 14:57:26 +0700

    Allow saving handover edit without new images

--- src/SmartCar.Web/ViewModels/RentalFlowViewModels.cs
+++ src/SmartCar.Web/ViewModels/RentalFlowViewModels.cs
@@ -183,11 +183,11 @@ public sealed class HandoverViewModel
     public List<IFormFile> Images { get; set; } = new();
 
     [Display(Name = "Ảnh mới bổ sung")]
-    public List<IFormFile> NewImages { get; set; } = new();
+    public List<IFormFile>? NewImages { get; set; }
 
     public List<string> ExistingImagePaths { get; set; } = new();
 
-    public List<string> ImagesToDelete { get; set; } = new();
+    public List<string>? ImagesToDelete { get; set; }
 
     [Range(0, int.MaxValue)]
     public int IncludedKilometers { get; set; }
