e352b45cfa3d7097f8613993972806e64ea63154
Trường Hoàng <113461904+truonghoangg@users.noreply.github.com>
2026-09-10 20:36:05 +0700

    separate staff operational permissions from admin

--- src/SmartCar.Web/Controllers/StaffController.cs
+++ src/SmartCar.Web/Controllers/StaffController.cs
@@ -14,7 +14,7 @@ using SmartCar.Web.ViewModels;
 
 namespace SmartCar.Web.Controllers;
 
-[Authorize(Roles = RoleNames.Staff + "," + RoleNames.Admin)]
+[Authorize(Roles = RoleNames.Staff)]
 public sealed class StaffController : Controller
 {
     private const string HandoverSignedMarker = "signed-handover-";
