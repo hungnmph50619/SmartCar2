d050fa4f94fb7b8485bc3c64d47528652035bfca
Trường Hoàng <113461904+truonghoangg@users.noreply.github.com>
2026-09-18 12:01:33 +0700

    fix: import pickup method in staff lookups

--- src/SmartCar.Web/Controllers/StaffLookupsController.cs
+++ src/SmartCar.Web/Controllers/StaffLookupsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SmartCar.Application.Features.Vehicles;
 using SmartCar.Domain.Constants;
+using SmartCar.Domain.Enums;
 using SmartCar.Infrastructure.Persistence;
 
 namespace SmartCar.Web.Controllers;
