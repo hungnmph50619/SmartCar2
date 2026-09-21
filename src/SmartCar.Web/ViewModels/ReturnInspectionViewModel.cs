29c4a2bdc96ca59ef403bc25bc84cebcc665830e
Trường Hoàng <113461904+truonghoangg@users.noreply.github.com>
2026-09-21 11:53:09 +0700

    Remove duplicate overdue settlement fields after merge

--- src/SmartCar.Web/ViewModels/ReturnInspectionViewModel.cs
+++ src/SmartCar.Web/ViewModels/ReturnInspectionViewModel.cs
@@ -36,13 +36,6 @@ public sealed class ReturnInspectionViewModel
 
     public decimal RefundAmount { get; init; }
 
-    public decimal OverdueCompensationDebtAmount { get; init; }
-
-    public bool OverdueCompensationDebtAwaitingConfirmation { get; init; }
-
-    public bool HasOpenOverdueCompensationDebt =>
-        OverdueCompensationDebtAmount > 0m;
-
     public IReadOnlyList<ChargeSummaryDto> AdditionalCharges { get; init; }
         = Array.Empty<ChargeSummaryDto>();
 
